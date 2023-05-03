3cc51e02b546db5a161a446945085160b17e060b
Dalf <105021624+dfran25@users.noreply.github.com>
2023-05-02 22:49:19 -0500

    Add files via upload

---
+++ gymEstudiantes/src/gymestudiantes/GymEstudiantes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gymestudiantes;
+
+
+
+public class GymEstudiantes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    jTable v1 = new jTable();
+    v1.setVisible(true);
+    }
+       
+}
